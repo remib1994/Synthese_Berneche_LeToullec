db14843c44d8735addc65d9081728c5264e247f6
Fridriver <117280045+Fridriver@users.noreply.github.com>
2023-05-18 11:04:35 -0400

    Vie fonctionnel / vie mechant / Ui en general

--- Assets/_MyAssets/Scripts/BarreDeVie.cs
+++ Assets/_MyAssets/Scripts/BarreDeVie.cs
@@ -5,22 +5,22 @@ using UnityEngine.UI;
 
 public class BarreDeVie : MonoBehaviour
 {
-    public Slider slider;
-    public Gradient gradient;
-    public Image jauge;
+    public Slider _slider;
+    public Gradient _gradient;
+    public Image _jauge;
 
     public void SetMaxHealth(int health)
     {
-        slider.maxValue = health;
-        slider.value = health;
+        _slider.maxValue = health;
+        _slider.value = health;
 
-        jauge.color = gradient.Evaluate(1f);
+        _jauge.color = _gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
-        slider.value = health;
+        _slider.value = health;
 
-        jauge.color = gradient.Evaluate(slider.normalizedValue);
+        _jauge.color = _gradient.Evaluate(_slider.normalizedValue);
     }
 }
